0ef01ce1d0aa0102731df5dda4d779f434cdb582
Ayodeji Onipe <ayosemail@gmail.com>
2021-12-30 22:20:59 -0500

    simple netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> near misses
+        <strong>{results.length}</strong> near (flying flaming rock) misses
       </p>
       <hr></hr>
       {results
